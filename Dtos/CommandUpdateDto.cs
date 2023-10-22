1b00ff1dc85eaaff50a40d77a5cd0b1367f40dff
Christian Oropeza <99064513+coropeza117@users.noreply.github.com>
2023-10-21 22:54:34 -0500

    Add files via upload

---
+++ Dtos/CommandUpdateDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Dtos
+{
+    public class CommandUpdateDto
+    {
+        // public int Id { get; set; } ... is created by our database no need to include it before hand
+        [Required]
+        [MaxLength(250)]
+        public string HowTo { get; set; }
+
+        [Required]  //  <--------------------------\
+        public string Line { get; set; }            //  -Gives us a 400 bad request error instead of 500 and detailed error feedback JSON text in Postman to troubleshoot                               
+
+        [Required]  //  <---------------------------/
+        public string Platform { get; set; }
+    }
+}
---
+++ Profiles/CommandsProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Commander.Dtos;
+using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            //...Source -> Target
+            //  Map between a source object & destination object - what we r mapping from and mapping to
+            CreateMap<Command, CommandReadDto>();
+            //        ^using Models; ^using Dtos;...Source -> Target
+
+            CreateMap<CommandCreateDto, Command>();
+
+            CreateMap<CommandUpdateDto, Command>();
+
+            CreateMap<Command, CommandUpdateDto>();
+
+        }
+    }
+}
